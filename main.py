d407c5d40e8db7ab56f171d4f4de0215bf88a9c3
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-02-21 10:41:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,22 +19,23 @@ Builder.load_string("""
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
-                
+        
         Button:
             font_size: 50
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
-            height: 100
-            text: "Exponents Calculator"
+            height: 200
+            text: "Tap anywhere to continue"
             on_release:
                 app.root.current = "Menu"
-                root.manager.transition.direction = "left"         
+                root.manager.transition.direction = "left"        
+
         Button:
             font_size: 50
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
             height: 100
-            text: "KSquared-math,LLC ©"
+            text: "KSquared-math,LLC © : Exponents Calculator"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
@@ -386,3 +387,4 @@ class Exponents(App):
 if __name__ == '__main__':
     Exponents().run()
     
+
